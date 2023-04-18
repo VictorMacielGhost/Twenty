5913b85e742f764cec7b9cc27468bf7e629dafbe
VictorMacielGhost <vtbolado17@gmail.com>
2023-04-18 05:12:31 -0300

    Basic form changed

--- website/index.php
+++ website/index.php
@@ -11,7 +11,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -20,7 +20,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <title>Twenty</title>
-
+    <script src="js/index.js"></script>
     <script>
 
         var userid = <?php echo ($_SESSION['userid']); ?>;
@@ -33,7 +33,7 @@
                 var request = new XMLHttpRequest();
                 request.open("GET", "php/make_reaction.php?postid=" + postid + "&ownerid=" + userid + "&type=" + reactiontype);
                 request.send();
-                var btn_like = document.getElementById("likes-count" + postid);
+                var btn_like = document.getElementById("icon-like" + postid);
                 request.onreadystatechange = () => 
                 {
                     btn_like.innerHTML = request.responseText;
@@ -45,7 +45,7 @@
                 var request = new XMLHttpRequest();
                 request.open("GET", "php/make_reaction.php?postid=" + postid + "&ownerid=" + userid + "&type=" + reactiontype);
                 request.send();
-                var btn_deslike = document.getElementById("deslikes-count" + postid);
+                var btn_deslike = document.getElementById("icon-deslike" + postid);
                 request.onreadystatechange = () =>
                 {
                     btn_deslike.innerHTML = request.responseText;
@@ -75,12 +75,27 @@
             }
             
         }
-    </script>
 
+        document.onkeyup = (e) =>
+        {
+            if(e.keyCode == 27 && isPostScreenOpened)
+            {
+                AppendPost();
+            }
+            else if(document.getElementById("posting").value == "")
+            {
+                document.getElementById("btn_post").setAttribute("disabled", "true");
+            }
+            else
+            {
+                document.getElementById("btn_post").removeAttribute("disabled");
+            }
+        }
+    </script>
 </head>
 <body>
 
-    <button class="btn-thinking" onclick="AppendPost()">What you thinking?</button>
+    <button class="btn-thinking" onclick="AppendPost()">Quer dizer algo?</button>
 
     <?php
 
@@ -103,10 +118,10 @@
             printf('<div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">%s</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                     <p class="card-text">%s</p>
-                    <a href="#" class="card-link">%d</a>
-                    <a href="#" class="card-link">%d</a>
+                    <button onclick="ToggleLike(' . $postid . ')" id="btn-like" ><i class="bi bi-heart" id="icon-like' . $postid . '"></i>%d</button>
+                    <button onclick="ToggleDeslike(' . $postid . '); " id="btn-deslike"><i class="bi bi-x-circle" id="icon-deslike' . $postid . '">%d</i></button>
+                    <button id="btn-comments"><i class="bi bi-megaphone"></i></button>
                 </div>
             </div>
             ', GetUserNameById($ownerid, $db_connection), $body, $likes, $deslikes);
@@ -116,10 +131,10 @@
     
     <div class="card-post" id="OpenPost">
         <div class="card" style="padding: 10px; border-radius: 20px;">
-            <i class="bi bi-x" style="font-size: 2rem;" onclick="AppendPost()"></i>
+            <i class="bi bi-x" style="font-size: 2rem;" onclick="AppendPost()" id="btn_close"></i>
             <form action="php/make_post.php" method="Post">
-                <textarea name="content" id="posting" cols="60" rows="7" placeholder="Teste" class="areapost" style="resize: none;"></textarea>
-                <input type="submit" value="Publish" class="btn btn-primary" style="margin-bottom: 10px;">
+                <textarea name="content" id="posting" cols="60" rows="5" placeholder="Oque você quer dizer?" class="areapost" style="resize: none;"></textarea>
+                <input type="submit" value="Publish" class="btn btn-primary" id="btn_post" style="margin-bottom: 10px;" disabled>
             </form>
             <div class="options-post">
                 <button class="op-post"><i class="bi bi-images"></i></button>
